53f520cb5d70ba9053e70b84bd7e5b620967d86d
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-14 01:36:18 +0500

    Add unit tests for expenses

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'John') }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expense = Expense.new(name: 'Groceries', amount: 50, user: user)
+      expect(expense).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      expense = Expense.new(amount: 50, user: user)
+      expect(expense).to be_invalid
+    end
+
+    it 'is invalid with a name shorter than 3 characters' do
+      expense = Expense.new(name: 'AB', amount: 50, user: user)
+      expect(expense).to be_invalid
+    end
+
+    it 'is invalid with a name longer than 30 characters' do
+      expense = Expense.new(name: 'A' * 31, amount: 50, user: user)
+      expect(expense).to be_invalid
+    end
+
+    it 'is invalid without an amount' do
+      expense = Expense.new(name: 'Groceries', user: user)
+      expect(expense).to be_invalid
+    end
+
+    it 'is invalid with a non-positive amount' do
+      expense = Expense.new(name: 'Groceries', amount: -10, user: user)
+      expect(expense).to be_invalid
+    end
+  end
 end
+
