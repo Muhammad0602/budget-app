24a07436afbe97bbdccaa77fc20c4579700d425a
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-13 22:44:00 +0500

    Add method for finding total amount

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories or /categories.json
   def index
     @categories = Category.where(author_id: current_user.id).order(created_at: :desc)
+    @total_amount = @category.expenses.sum(:amount)
   end
 
   # GET /categories/1 or /categories/1.json
